2d17cc7be83dd026a3b4562a41f157f361f3a8fe
Carter Green <cartergr@usc.edu>
2016-07-11 00:32:48 -0700

    Improved Twitter stream
    Tracks first 400 stocks in the S&P 500 alphabetically; this will be
    changed to the 400 most discussed at a later date. Also created better
    error handling and logging. Is now integrated with the MySQL backend.

--- twitter_stream.py
+++ twitter_stream.py
@@ -3,44 +3,8 @@ from tweepy import Stream
 from tweepy.streaming import StreamListener
 import json
 import time
-import sys
-import mysql_backend
-
-
-class Listener(StreamListener):
-    # def __init__(self):
-    #     StreamListener.__init__(self)
-    #     # self.cnx, self.cursor = mysql_backend.connect_db()
-
-    def on_data(self, data):
-        # try:
-            # mysql_backend.insert_tweet(self.cnx, self.cursor,
-            #                            data.user.encoding("utf-8"),
-            #                            data.user.time("utf-8"),
-            #                            data.text.encoding("utf-8"),
-            #                            data.text)
-        data = json.loads(data)
-        # filter out retweets because they're duplicates
-        try:
-            if data["text"][0:2] != "RT":
-                print("\n" + data["text"])
-            return True
-        except KeyError:
-            return True
-
-        #except ConnectionError:
-            # Create better error catching and logging
-         #   pass
-
-        # return True
-
-    def on_error(self, status_code):
-        print(status_code)
-        return False
-
-    def on_timeout(self):
-        time.sleep(30)
-        return
+from time import strftime
+import mysql_backend as mb
 
 
 def twitter_streaming(track):
@@ -57,6 +21,66 @@ def twitter_streaming(track):
     stream.filter(track=track, languages=["en"])
 
 
+class Listener(StreamListener):
+    # def __init__(self):
+    #     StreamListener.__init__(self)
+    #     # self.cnx, self.cursor = mysql_backend.connect_db()
+    def __init__(self):
+        super().__init__()
+        # self.fout = open("S&P stream.txt", "a")
+        # self.fout2 = open("S&P stream full.txt", "a")
+        self.error_out = open("DB import Errors.txt", "a")
+        self.cnx, self.cursor = mb.connect_db()
+
+    def on_data(self, data):
+        data = json.loads(data)
+        try:
+            tweet_id = data["id"]
+            language = data["lang"]
+            tweet_time = data["timestamp_ms"]
+            text = data["text"]
+            stocks = data["entities"]["symbols"]
+            user_screen_name = data["user"]["screen_name"]
+            location = data["user"]["location"]
+            follower_count = data["user"]["followers_count"]
+
+            mb.insert_tweet(self.cnx, self.cursor,
+                            tweet_id, language, tweet_time, text, stocks, user_screen_name,
+                            location, follower_count)
+        # try:
+        #     if data["text"][0:2] != "RT":
+        #         print("\n" + strftime("%Y-%m-%d %H:%M:%S") + "\n" + data["text"], file=Listener.fout)
+        #         print(json.dumps(data, indent=4, sort_keys=True), file=Listener.error_out)
+        #     return True
+        except KeyError:
+            print("Key error | " + strftime("%Y-%m-%d %H:%M:%S"))
+            return
+        # Certain characters like emojis aren't working properly and are causing encoding errors
+        except UnicodeEncodeError:
+            print("\n" + strftime("%Y-%m-%d %H:%M:%S") + "\nUnicode error", file=self.error_out)
+            print("Unicode error | " + strftime("%Y-%m-%d %H:%M:%S"))
+            return True
+        except ConnectionError:
+            print("Connection error | " + strftime("%Y-%m-%d %H:%M:%S"))
+            try:
+                print("\nConnection error\n" + json.dumps(data, indent=4, sort_keys=True), file=self.error_out)
+            except UnicodeEncodeError:
+                print("\n" + strftime("%Y-%m-%d %H:%M:%S") + "\nUnicode error", file=self.error_out)
+            return True
+
+    def on_error(self, status_code):
+        print("Error status code: " + str(status_code)+ " | " + strftime("%Y-%m-%d %H:%M:%S"))
+        print("\n" + strftime("%Y-%m-%d %H:%M:%S") + "\nError status code: " + str(status_code), file=self.error_out)
+        return
+
+    def on_timeout(self):
+        # On Twitter connection timeout, retry in a minute
+        print("Twitter connection timeout")
+        print("\n" + strftime("%Y-%m-%d %H:%M:%S") + "\n""Twitter connection timeout", file=self.error_out)
+        time.sleep(60)
+        return
+
+
 if __name__ == "__main__":
     stock_symbols = []
 
@@ -65,9 +89,6 @@ if __name__ == "__main__":
             for index, column in enumerate(line.split(",")):
                 if index == 0:
                     stock_symbols.append("$" + column)
-
-    first_half = stock_symbols[:(len(stock_symbols) // 2)]
-    second_half = stock_symbols[(len(stock_symbols) // 2):]
 
     twitter_streaming(stock_symbols[:400])
 
